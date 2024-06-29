32d0b8deec6803fefb4335ec2e19a6b467003692
ArminMirzaloo <arminmirzaloo426@gmail.com>
2024-06-29 15:00:23 +0330

    Add files via upload

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,17 @@
-//
-// Created by manin on 6/29/2024.
-//
-
 #include "Game.h"
+
+Game::Game() {
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    showFullScreen();
+
+    auto scene = new QGraphicsScene(this);
+    scene->setSceneRect( 0 , 0 , width() , height() );
+
+
+    scene->setBackgroundBrush(QBrush(QColor("#00008B")));
+
+    setScene(scene);
+
+
+}
