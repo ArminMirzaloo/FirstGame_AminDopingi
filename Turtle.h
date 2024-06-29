46fcc0f9460c56bef25688b9ede2a7c644ed77e9
maninoj <maninajafi1384@gmail.com>
2024-06-29 12:42:27 +0430

    make Turtle calss in this branch

---
+++ Turtle.h
@@ -0,0 +1,16 @@
+//
+// Created by manin on 6/29/2024.
+//
+
+#ifndef FIRSTGAME_AMIN_DOPINGI__TURTLE_H
+#define FIRSTGAME_AMIN_DOPINGI__TURTLE_H
+
+
+
+class Turtle {
+
+};
+
+
+
+#endif //FIRSTGAME_AMIN_DOPINGI__TURTLE_H
